658e416bad2b97abdbc9c8ffec5d81053b7daaa3
wangzhonghua <wangzhonghua@devairdoc.ns>
2025-02-28 12:16:06 +1100

    u

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@ python train.py -net sam -mod sam_adpt -exp_name oc_new -sam_ckpt ./checkpoint/s
 python train.py -net sam -mod sam_adpt -exp_name ex_new -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset ex -data_path custom
 python train.py -net sam -mod sam_adpt -exp_name he_new -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset he -data_path custom
 
-python val.py -net sam -mod sam_adpt -exp_name vessel_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weight ./logs/vessel_new/Model/best_dice_checkpoint.pth
-python val.py -net sam -mod sam_adpt -exp_name od_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weight ./logs/vessel_new/Model/best_dice_checkpoint.pth
-python val.py -net sam -mod sam_adpt -exp_name oc_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weight ./logs/vessel_new/Model/best_dice_checkpoint.pth
-python val.py -net sam -mod sam_adpt -exp_name ex_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weight ./logs/vessel_new/Model/best_dice_checkpoint.pth
-python val.py -net sam -mod sam_adpt -exp_name he_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weight ./logs/vessel_new/Model/best_dice_checkpoint.pth
+python val.py -net sam -mod sam_adpt -exp_name vessel_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weights ./logs/vessel_new/Model/best_dice_checkpoint.pth
+python val.py -net sam -mod sam_adpt -exp_name od_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weights ./logs/vessel_new/Model/best_dice_checkpoint.pth
+python val.py -net sam -mod sam_adpt -exp_name oc_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weights ./logs/vessel_new/Model/best_dice_checkpoint.pth
+python val.py -net sam -mod sam_adpt -exp_name ex_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weights ./logs/vessel_new/Model/best_dice_checkpoint.pth
+python val.py -net sam -mod sam_adpt -exp_name he_new_eval -sam_ckpt ./checkpoint/sam/sam_vit_b_01ec64.pth -image_size 1024 -b 2 -val_freq 1 -dataset relabel -data_path custom -weights ./logs/vessel_new/Model/best_dice_checkpoint.pth
